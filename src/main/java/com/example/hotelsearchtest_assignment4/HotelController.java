e3324fd04cc6b7afcfb0289ae85a01e0b5b7b768
thorsanchez <ths281@hi.is>
2024-03-23 18:35:34 +0100

    update2

--- src/main/java/com/example/hotelsearchtest_assignment4/HotelController.java
+++ src/main/java/com/example/hotelsearchtest_assignment4/HotelController.java
@@ -1,6 +1,27 @@
 package com.example.hotelsearchtest_assignment4;
 
+import java.time.LocalDate;
 
 public class HotelController {
 
+    private Hotel[] hotels; // listi af hotelum
+
+    public HotelController(Hotel[] hotels) {
+        this.hotels = hotels;
+    }
+
+    public void handleHotelSelection() {
+        // Logic til að velja hotel frá fylki (af hotelum)
+    }
+
+    public Hotel searchHotel(LocalDate date) {
+        // Notum searchHotel sem skilar laust hotel með laust herbergi fyrir gefna dagsetningu 
+        for (Hotel hotel : hotels) {
+            if (hotel.getAvailability(date) > 0) {
+                return hotel;
+            }
+        }
+        return null; // Engin laus hotel?
+    }
+    
 }
